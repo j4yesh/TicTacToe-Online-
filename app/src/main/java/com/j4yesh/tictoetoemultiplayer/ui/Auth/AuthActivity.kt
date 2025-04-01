8820444fbff16176f87af9b9ada4a3d49ac59c1b
j4yesh <jayeshbadgujar7052@gmail.com>
2025-04-02 04:59:30 +0530

    bug fix

--- app/src/main/java/com/j4yesh/tictoetoemultiplayer/ui/Auth/AuthActivity.kt
+++ app/src/main/java/com/j4yesh/tictoetoemultiplayer/ui/Auth/AuthActivity.kt
@@ -1,15 +1,16 @@
 package com.j4yesh.tictoetoemultiplayer.ui.Auth
 
 import android.os.Bundle
-import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.j4yesh.tictoetoemultiplayer.R
 
+class AuthActivity : AppCompatActivity() {
 
-class AuthActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Toast.makeText(this, "inside auth activity",Toast.LENGTH_LONG).show()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.fragment_login)
     }
 }
